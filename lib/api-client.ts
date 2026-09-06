bed86ac4002f7a2a373468abfd1c8624c0336ff6
Tanzeem74 <tanzeemafsar74@gmail.com>
2026-09-06 23:20:41 +0600

    finalize the project

--- lib/api-client.ts
+++ lib/api-client.ts
@@ -1,36 +1,40 @@
-import axios from 'axios';
-import { getAccessToken, clearAuth } from './auth-helper';
+import axios from "axios";
+import { getAccessToken, clearAuth } from "./auth-helper";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use(
-    (config) => {
-        const token = getAccessToken();
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
+  (config) => {
+    const token = getAccessToken();
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
 );
 
 api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            if (typeof window !== 'undefined') {
-                clearAuth();
-                // eslint-disable-next-line @next/next/no-location-assign-relative-destination
-                window.location.href = '/login';
-            }
-        }
-        return Promise.reject(error);
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      if (typeof window !== "undefined") {
+        clearAuth();
+
+        // eslint-disable-next-line @next/next/no-location-assign-relative-destination
+        window.location.href = "/login";
+      }
     }
+
+    return Promise.reject(error);
+  },
 );
 
 export default api;
